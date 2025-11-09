f6d38d7968cf5b2e35211045147f30a8a7361050
Avik Hasan <ah.avik.hasan@gmail.com>
2025-11-09 23:22:33 +0600

    Implement main menu for workspace management

---
+++ AdiminWorkspace.c
@@ -0,0 +1,36 @@
+int main() {
+    void (*menu_functions[])() = {
+        addMember,
+        viewAllMembers,
+        removeMember,
+        createEvent,
+        viewAllEvents,
+        cancelEvent
+    };
+
+    int choice;
+
+    do {
+        system("cls || clear");
+        showMenu();
+        printf("Enter your choice (0-6): ");
+        
+        if (scanf("%d", &choice) != 1) {
+            choice = -1;
+        }
+        clearInputBuffer();
+
+        if (choice >= 1 && choice <= 6) {
+            menu_functions[choice - 1]();
+        } else if (choice == 0) {
+            printf("Thank you... Goodbye!\n");
+        } else {
+            printf("Invalid choice. Please try again.\n");
+        }
+        if (choice != 0) {
+            printf("\nPress Enter to return to the menu...");
+            getchar();
+        }
+    } while (choice != 0);
+    return 0;
+}
